51f9edb920a524a10a95138d42ff3b57876c1d37
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-29 15:32:44 +0300

    add chat engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
 function App() {
   return (
     <div className="App">
-     <h1>Chat App</h1>
+      <ChatEngine
+        height="100vh"
+        projectID="0c033c07-4731-40fc-bef6-b961ecd3376f"
+        userName="odhiambo.edward111@gmail.com"
+        password="12900001"
+      />
     </div>
   );
 }
