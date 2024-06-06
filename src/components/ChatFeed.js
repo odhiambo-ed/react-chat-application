a038a067f1852ad935099c99fe81b0ee7c14c259
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-06 23:42:03 +0300

    add chatfeed function

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -1,12 +1,42 @@
 import React from 'react'
+import MyMessage from './MyMessage'
+import TheirMessage from './TheirMessage'
+import MessageForm from './MessageForm'
 
 const ChatFeed = (props) => {
-  console.log(props);
-  return (
-      <div>
-          Chat Feed
-    </div>
-  )
+  const { chats, activeChat, userName, messages } = props;
+  const chat = chats && chats[activeChat];
+
+  const renderMessages = () => 
+  {
+    const keys = Object.keys(messages);
+
+    return keys.map((key, index) => 
+    {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: '100%' }}>
+          <div className="message-block">
+            {
+              isMyMessage
+              ? <MyMessage message={message} />
+              : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />
+            }
+          </div>
+          <div
+            className="read-receipts"
+            style={{
+              marginRight: isMyMessage ? '18px' : '0px',
+              marginLeft: isMyMessage ? '0px' : '68px'
+            }}
+          ></div>
+        </div>
+      );
+    });
+  }
 }
 
 export default ChatFeed
