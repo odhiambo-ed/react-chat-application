954740b2ccb65960fcc6bcde9dfa911fc54bc5ca
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-06 23:46:04 +0300

    render chat

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -37,6 +37,21 @@ const ChatFeed = (props) => {
       );
     });
   }
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat?.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => `${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: '100px' }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
+  )
 }
 
 export default ChatFeed
