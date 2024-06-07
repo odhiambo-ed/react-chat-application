cfccf5300a6c5c1799dae589d514bce4c51be4fa
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-07 23:01:49 +0300

    edit app

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
 import Login from './components/Login';
 
+const projectID = '0c033c07-4731-40fc-bef6-b961ecd3376f'
+
 function App() {
   if (!localStorage.getItem('username')) return <Login />;
   return (
     <div className="App">
       <ChatEngine
         height="100vh"
-        projectID="0c033c07-4731-40fc-bef6-b961ecd3376f"
-        userName="odhiambo-ed"
-        userSecret="12900001"
+        projectID={projectID}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
       />
