e8971381a780585a03ace97dd2f6a1d3eaf69dd2
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-07 22:13:16 +0300

    add login form file

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const projectID = '0c033c07-4731-40fc-bef6-b961ecd3376f'
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
