a37258e6fabc63f49201023174cc794a0478eea1
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-07 21:33:19 +0300

    import icons

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { sendMessage, isTyping } from 'react-chat-engine'
+import { SendOutlined, PictureOutlined } from '@ant-design/icons'
 
-function MessageForm() {
+
+
+
+function MessageForm({props}) {
     return (
         <div>
             MessageForm
