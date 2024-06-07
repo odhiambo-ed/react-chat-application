3308519c88b57f08871d2f2870bd08b930e67ebb
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-07 22:25:46 +0300

    add login auth function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,56 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { hasFormSubmit } from '@testing-library/user-event/dist/utils'
 
 const projectID = '0c033c07-4731-40fc-bef6-b961ecd3376f'
 
 function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password }
+        try {
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject })
+            localStorage.setItem('username', username)
+            localStorage.setItem('password', password)
+            window.location.reload()
+        } catch (error) {
+            setError('Oops, incorrect credentials.')
+        }
+    }
   return (
-    <div>Login</div>
+      <div className="wrapper">
+        <div className="form">
+          <h1 className="title">Chat Application</h1>
+          <form>
+            <div className="field">
+              <input
+                type="text"
+                value=""
+                className="input"
+                          placeholder="Username"
+                          onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="field">
+              <input
+                type="password"
+                value=""
+                className="input"
+                          placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="field">
+              <button className="button" onSubmit={handleSubmit}>
+                <span>Start chatting</span>
+              </button>
+            </div>
+          </form>
+        </div>
+    </div>
   )
 }
 
