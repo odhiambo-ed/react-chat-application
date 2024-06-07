24e036b4cb6d1aaedd6fb853c379bd80ad4b8574
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-07 21:41:39 +0300

    add message form

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { sendMessage, isTyping } from 'react-chat-engine'
 import { SendOutlined, PictureOutlined } from '@ant-design/icons'
+import { hasFormSubmit } from '@testing-library/user-event/dist/utils';
 
 
 function MessageForm({ props }) {
@@ -8,9 +9,30 @@ function MessageForm({ props }) {
     const { chatId, creds } = props;
 
     return (
-        <div>
-            MessageForm
-        </div>
+        <form className="message-form" onSubmit={hasFormSubmit}>
+            <input
+                className="message-input"
+                placeholder="Send a message..."
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+                onSubmit={hasFormSubmit}
+            />
+            <label htmlFor="upload-button">
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon" />
+                </span>
+            </label>
+            <input
+                type="file"
+                multiple={false}
+                id="upload-button"
+                style={{ display: 'none' }}
+                onChange={handleUpload}
+            />
+            <button type="submit" className="send-button" onClick={handleSendMessage}>
+                <SendOutlined className="send-icon" />
+            </button>
+        </form>
     )
 }
 
